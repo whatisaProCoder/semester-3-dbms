03fad360621a854cc49489f64e287ade508cb46a
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-07 15:50:37 +0530

    final

--- class_01/program_01/script.sql
+++ class_01/program_01/script.sql
@@ -4,29 +4,29 @@ create database db;
 use db;
 
 create table student (
-  id varchar(10) primary key,
+  id int primary key,
   name varchar(100),
   city varchar(100)
 );
 
 insert into student (id, name, city) values (
-  'CJ101', 'Deepak', 'Mumbai'
+  101, 'Deepak', 'Mumbai'
 );
 
 insert into student (id, name, city) values (
-  'CJ102', 'Aman', 'Bangalore'
+  102, 'Aman', 'Bangalore'
 );
 
 insert into student (id, name, city) values (
-  'CJ103', 'Rajkumar', 'Kolkata'
+  103, 'Rajkumar', 'Kolkata'
 );
 
 insert into student (id, name, city) values (
-  'CJ104', 'Aman', 'Delhi'
+  104, 'Aman', 'Delhi'
 );
 
 insert into student (id, name, city) values (
-  'CJ105', 'Kunal', 'Mumbai'
+  105, 'Kunal', 'Mumbai'
 );
 
 describe student;
