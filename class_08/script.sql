d1bc21d9a706784c8825c8fb9ef675733001a4f1
whatisaProCoder <debnathpritam2005@gmail.com>
2025-11-12 20:57:23 +0530

    fix

--- class_08/script.sql
+++ class_08/script.sql
@@ -8,16 +8,15 @@ create view COMEDY_MOVIES as select MV_NO, TITLE, STAR, PRICE from MOVIE where T
 
 
 -- Find out the names of comedy movies using the above view.
-select * from COMEDY_MOVIES;
+select TITLE from COMEDY_MOVIES;
 
 -- Create a view comedy movie price with title, price of movie table for only 
 -- comedy movies. 
 create view COMEDY_MOVIE_PRICE as select TITLE, PRICE from MOVIE where TYPE = 'comedy';
 
 -- Find out the price of comedy movie home alone from the above view.
-select * from COMEDY_MOVIE_PRICE;
+select PRICE from COMEDY_MOVIE_PRICE where TITLE = 'home alone';
 
 -- Create a view movie type wise total price with two attributes named as movie type 
 -- and total price from movie table for each movie. 
 create view MOVIE_TYPE_WITH_TOTAL_PRICE as select TYPE, sum(PRICE) as TOTAL_PRICE from MOVIE group by TYPE;
-select * from MOVIE_TYPE_WITH_TOTAL_PRICE;
